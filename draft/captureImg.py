5e90ab8d4a50cde341977004c28cacca5552dd73
Xiwei (Rosalie) Ma <xiwei@ucla.edu>
2018-02-17 15:54:47 -0800

    add 2 options

---
+++ draft/captureImg.py
@@ -0,0 +1,97 @@
+import cv2
+import numpy as np
+
+
+#save = False
+#numberPerGesture = 
+
+#def saveImage(img):
+#    save=False
+x1=150
+y1=50
+h1=150
+w1=150
+
+
+showVideo = True
+methodMask = True
+selectRec = True
+betterQuality = False
+waitTime = 500
+
+def Mask(img):
+    if selectRec:
+        cv2.rectangle(img,(x1,y1),(x1+w1,y1+h1),(255,0,0),1)
+        img = img[y1:y1+h1, x1:x1+w1]
+
+    lowerBound = np.array([0, 0, 60])
+    upperBound = np.array([10, 200, 255])
+    
+    #kernelOpen=np.ones((5,5))
+    kernelClose= np.ones((15,15))
+
+    #convert BGR to HSV
+    imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+    #create mask
+    mask=cv2.inRange(imgHSV,lowerBound,upperBound)
+
+    #morphology
+    #mask=cv2.morphologyEx(mask, cv2.MORPH_OPEN,kernelOpen)
+    mask=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernelClose)
+
+    mask = cv2.GaussianBlur(mask, (15,15), 1)
+
+    if betterQuality:
+        kernelBettter=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+        mask = cv2.erode(mask, kernelBettter, iterations = 1)
+        mask = cv2.dilate(mask, kernelBettter, iterations = 1)
+    
+    #bitwise and mask original frame
+    imgBit = cv2.bitwise_and(img, img, mask = mask)
+
+    # color to grayscale
+    imgBit = cv2.cvtColor(imgBit, cv2.COLOR_BGR2GRAY)
+    #drawing rectangle over objects
+
+#    if save == True:
+#        saveImage(imgBit)
+
+    return imgBit
+    
+def alternative(img):
+    if selectRec:
+        cv2.rectangle(img,(x1,y1),(x1+w1,y1+h1),(255,0,0),1)
+        img = img[y1:y1+h1, x1:x1+w1]
+    maskGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    maskBlur = cv2.GaussianBlur(maskGray,(5,5),1)
+    maskThs = cv2.adaptiveThreshold(maskBlur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+    ret,maskFinal = cv2.threshold(maskThs, 70, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    return maskFinal
+
+def main():
+    cam= cv2.VideoCapture(0)
+    ret = cam.set(3,72)
+    ret = cam.set(4,64)
+
+    while True:
+        ret, img=cam.read()
+        img = cv2.flip(img,3)
+
+        if ret :
+            if methodMask:
+                mask=Mask(img)
+            else:
+                mask=alternative(img)
+        if showVideo:
+            cv2.imshow("mask",mask)
+            cv2.imshow("Original",img)
+        
+        cv2.waitKey(waitTime)
+
+
+if __name__ == "__main__":
+    main()
+
+        
+    
